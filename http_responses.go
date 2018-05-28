e2d1df93120fb7a174662fa4f6c9c36dae41b502
Aaron Hill <68696c6c@gmail.com>
2018-05-28 11:30:29 -0600

    Added binding and response helpers.

--- http_responses.go
+++ http_responses.go
@@ -1,14 +1,40 @@
 package goat
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+	"reflect"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/go-playground/validator.v8"
 )
 
+// A generic response.
+// swagger:response Response
 type Response struct {
 	Message string                 `json:"message"`
 	Errors  []string               `json:"errors,omitempty"`
 	Data    map[string]interface{} `json:"data,omitempty"`
+}
+
+// A validation error response.
+// swagger:response ValidationResponse
+type ValidationResponse struct {
+	Message string            `json:"message"`
+	Errors  map[string]string `json:"errors,omitempty"`
+}
+
+// A boolean response.
+// swagger:response BoolResponse
+type BoolResponse struct {
+	Valid bool `json:"valid"`
+}
+
+func RespondValid(c *gin.Context) {
+	c.JSON(http.StatusOK, BoolResponse{true})
+}
+
+func RespondInvalid(c *gin.Context) {
+	c.JSON(http.StatusBadRequest, BoolResponse{false})
 }
 
 func RespondMessage(c *gin.Context, m string) {
@@ -26,13 +52,23 @@ func RespondCreated(c *gin.Context, data interface{}) {
 	c.Abort()
 }
 
-func RespondNotFoundError(c *gin.Context, errs []error) {
-	c.JSON(http.StatusOK, Response{"Not found.", ErrorsToStrings(errs), nil})
+func RespondNotFoundError(c *gin.Context, err error) {
+	c.JSON(http.StatusNotFound, Response{"Not found.", []string{err.Error()}, nil})
 	c.Abort()
 }
 
-func RespondBadRequestError(c *gin.Context, errs []error) {
+func RespondNotFoundErrors(c *gin.Context, errs []error) {
+	c.JSON(http.StatusNotFound, Response{"Not found.", ErrorsToStrings(errs), nil})
+	c.Abort()
+}
+
+func RespondBadRequestErrors(c *gin.Context, errs []error) {
 	c.JSON(http.StatusBadRequest, Response{"Bad Request.", ErrorsToStrings(errs), nil})
+	c.Abort()
+}
+
+func RespondBadRequestError(c *gin.Context, err error) {
+	c.JSON(http.StatusBadRequest, Response{"Bad Request.", []string{err.Error()}, nil})
 	c.Abort()
 }
 
@@ -41,8 +77,39 @@ func RespondBadRequest(c *gin.Context, data interface{}) {
 	c.Abort()
 }
 
-func RespondUnauthorizedError(c *gin.Context, errs []error) {
-	c.JSON(http.StatusUnauthorized, Response{"Unauthorized.", ErrorsToStrings(errs), nil})
+func RespondValidationError(c *gin.Context, errs map[string]error) {
+	msgs := make(map[string]string)
+	for k, v := range errs {
+		msgs[k] = v.Error()
+	}
+	c.JSON(http.StatusBadRequest, ValidationResponse{"Invalid Request.", msgs})
+	c.Abort()
+}
+
+func RespondRequestValidationError(c *gin.Context, err error, t reflect.Type) {
+	msgs := make(map[string]string)
+
+	ve, ok := err.(validator.ValidationErrors)
+	if !ok {
+		c.JSON(http.StatusBadRequest, Response{"Invalid Request.", []string{}, nil})
+		c.Abort()
+		return
+	}
+	for _, e := range ve {
+		jsonName, label, err := GetStructFieldValidationMeta(t, e)
+		if err != nil {
+			jsonName = e.Field
+			label = e.Name
+		}
+
+		msgs[jsonName] = label + " is required"
+	}
+	c.JSON(http.StatusBadRequest, ValidationResponse{"Invalid Request.", msgs})
+	c.Abort()
+}
+
+func RespondUnauthorizedError(c *gin.Context) {
+	c.JSON(http.StatusUnauthorized, Response{"Unauthorized.", []string{}, nil})
 	c.Abort()
 }
 
@@ -51,7 +118,20 @@ func RespondAuthenticationError(c *gin.Context) {
 	c.Abort()
 }
 
-func RespondServerError(c *gin.Context, errs []error) {
+func RespondServerErrors(c *gin.Context, errs []error) {
+	// Only show errors to the user if we are in debug mode.
+	if gin.Mode() != gin.DebugMode {
+		errs = []error{}
+	}
 	c.JSON(http.StatusInternalServerError, Response{"Internal server error.", ErrorsToStrings(errs), nil})
+	c.Abort()
+}
+
+func RespondServerError(c *gin.Context, err error) {
+	// Only show errors to the user if we are in debug mode.
+	if gin.Mode() != gin.DebugMode {
+		err = nil
+	}
+	c.JSON(http.StatusInternalServerError, Response{"Internal server error.", []string{err.Error()}, nil})
 	c.Abort()
 }
