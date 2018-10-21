bd51b05fe217b04408250a2240380ae5f38cb3e5
Aaron Hill <68696c6c@gmail.com>
2018-10-21 07:23:28 -0600

    Added a TimeToPrettyString helper and time helper tests.

--- time.go
+++ time.go
@@ -1,6 +1,9 @@
 package goat
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 const (
 	ymdTemplate    = "2006-01-02"
@@ -21,4 +24,20 @@ func YMDStringToTime(str string) (time.Time, error) {
 
 func TimeToYMDString(t time.Time) string {
 	return t.Format(ymdTemplate)
+}
+
+func TimeToPrettyString(t time.Time) string {
+	suffix := "th"
+	switch t.Day() {
+	case 1, 21, 31:
+		suffix = "st"
+	case 2, 22:
+		suffix = "nd"
+	case 3, 23:
+		suffix = "rd"
+	}
+	m := t.Month().String()
+	d := t.Day()
+	y := t.Year()
+	return fmt.Sprintf("%s %d%s %d", m, d, suffix, y)
 }
