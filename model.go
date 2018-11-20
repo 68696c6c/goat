34fc65a4d0ff916d67a59aec2822d9eaf9e7762b
Aaron Hill <68696c6c@gmail.com>
2018-11-20 10:33:33 -0700

    Made models use the goat ID type.

--- model.go
+++ model.go
@@ -2,12 +2,10 @@ package goat
 
 import (
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type Model struct {
-	ID        uuid.UUID  `json:"id" gorm:"primary_key"`
+	ID        ID         `json:"id" gorm:"primary_key"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at"`
 	DeletedAt *time.Time `json:"deleted_at"`
