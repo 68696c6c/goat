8e66692a6b9e28b239c098bb036ed257a9b42dd4
Aaron Hill <68696c6c@gmail.com>
2020-05-28 15:44:44 -0600

    Made the RouterGin a pointer to fix a bug where setting a router config did nothing.

--- src/http/router.go
+++ src/http/router.go
@@ -22,20 +22,20 @@ type RouterGin struct {
 	config  map[string]interface{}
 }
 
-func NewRouterGin(host, port string) RouterGin {
-	return RouterGin{
+func NewRouterGin(host, port string) *RouterGin {
+	return &RouterGin{
 		host:    host,
 		port:    port,
 		address: fmt.Sprintf("%s:%s", host, port),
 	}
 }
 
-func (r RouterGin) GetEngine() *gin.Engine {
+func (r *RouterGin) GetEngine() *gin.Engine {
 	return r.Engine
 }
 
 // Run the Gin engine.
-func (r RouterGin) Run() error {
+func (r *RouterGin) Run() error {
 	if err := validPort(r.port); err != nil {
 		return errors.Wrap(err, "failed to start router")
 	}
@@ -44,13 +44,13 @@ func (r RouterGin) Run() error {
 
 // Set a map of key-value pairs that will be added to the Gin registry when the
 // router initializes.
-func (r RouterGin) SetRegistry(d map[string]interface{}) {
+func (r *RouterGin) SetRegistry(d map[string]interface{}) {
 	r.config = d
 }
 
 // Copy router config values to the Gin registry where they can be accessed by
 // the app.
-func (r RouterGin) InitRegistry() gin.HandlerFunc {
+func (r *RouterGin) InitRegistry() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		for key, value := range r.config {
 			c.Set(key, value)
