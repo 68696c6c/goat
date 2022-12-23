0281dfc9f42016fe5c2fd28f86912bf3874efa8d
Aaron Hill <68696c6c@gmail.com>
2022-12-23 10:46:06 -0700

    Update the RecordNotFound error helpers to work on single errors since Gorm v2 no longer returns error arrays.

--- errors.go
+++ errors.go
@@ -32,21 +32,12 @@ func ExitSuccess() {
 	utils.ExitSuccess()
 }
 
-// Returns true if the provided slice of errors contains a GORM "record not found" error.
-func RecordNotFound(errs []error) bool {
-	return utils.RecordNotFound(errs)
+// RecordNotFound returns true if the provided error is the Gorm "record not found" error.
+func RecordNotFound(err error) bool {
+	return utils.RecordNotFound(err)
 }
 
-// Returns true if there are any errors in the provided array that are NOT a GORM "record not found" error.
-func ErrorsBesidesRecordNotFound(errs []error) bool {
-	return utils.ErrorsBesidesRecordNotFound(errs)
-}
-
-// Returns true if the provided error is the GORM "record not found" error.  Note that this function only returns true
-// on exact matches, so wrapped or flattened errors containing the "record not found" error will not be caught.  Given
-// that the GORM "record not found" error is rather generic, handling those cases is too likely to result in false
-// positives.  For this reason, the check should be performed before wrapping or flattening any GORM errors using the
-// other helper functions in this file.
-func IsNotFoundError(err error) bool {
-	return utils.IsNotFoundError(err)
+// ErrorBesidesRecordNotFound true if the provided error is NOT a Gorm "record not found" error.
+func ErrorBesidesRecordNotFound(err error) bool {
+	return utils.ErrorBesidesRecordNotFound(err)
 }
